175f80d2d8329e4adf60248b7a91428566abd590
Alizah Lalani <alizah.lalani@gmail.com>
2018-11-20 18:50:20 -0500

    added camera controls

--- client/components/Board.js
+++ client/components/Board.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import * as THREE from "three";
 import { createCube, createBlankTile } from "./renderFuncs/createTile";
 const OrbitControls = require("three-orbit-controls")(THREE);
+//const MapControls = require("three-map-controls")(THREE);
 import CurrentTile from "./CurrentTile";
 import checkValid from "./renderFuncs/checkValid";
 import {connect} from 'react-redux'
@@ -17,6 +18,7 @@ class Board extends Component {
     this.initializeOrbits = this.initializeOrbits.bind(this);
     this.updateValidTiles = this.updateValidTiles.bind(this);
     this.resetCamera = this.resetCamera.bind(this);
+    this.threeDcamera = this.threeDcamera.bind(this);
   }
 
   componentDidMount() {
@@ -27,6 +29,9 @@ class Board extends Component {
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    //this.controls = new MapControls(this.camera, this.renderer.domElement);
+    this.controlCenter = this.controls.center.clone();
+    this.controlRotation = this.camera.rotation.clone();
 
     this.renderer.setSize(width, height);
     this.mount.appendChild(this.renderer.domElement);
@@ -88,7 +93,13 @@ class Board extends Component {
     this.controls.rotateSpeed = 1.0;
     this.controls.zoomSpeed = 1.2;
     this.controls.panSpeed = 0.8;
+    this.controls.mouseButtons = {
+      ORBIT: THREE.MOUSE.RIGHT,
+      ZOOM: THREE.MOUSE.MIDDLE,
+      PAN: THREE.MOUSE.LEFT
+    };
   }
+
 
   initializeCamera() {
     this.camera.position.x = 0;
@@ -125,12 +136,23 @@ class Board extends Component {
     this.camera.position.x = 0;
     this.camera.position.y = 0;
     this.camera.position.z = 4;
-    this.camera.lookAt(new THREE.Vector3(0,0, 0))
+    this.camera.lookAt(new THREE.Vector3(0,0,0))
+    this.controls.center.set(this.controlCenter.x, this.controlCenter.y, this.controlCenter.z);
+    this.camera.rotation.set(this.controlRotation.x, this.controlRotation.y, this.controlRotation.z);
+  }
+
+  threeDcamera () {
+    this.camera.position.x = 0;
+    this.camera.position.y = -6;
+    this.camera.position.z = 2;
+    this.camera.lookAt(new THREE.Vector3(0,0,0));
+    this.controls.center.set(this.controlCenter.x, this.controlCenter.y, this.controlCenter.z);
   }
   render() {
     return (
       <div>
-        <button type="button" onClick={this.resetCamera}> Move Camera </button>
+        <button type="button" onClick={this.resetCamera}> Flat Board </button>
+        <button type="button" onClick={this.threeDcamera}> 3D Board </button>
         <div
           onClick={e => this.onDocMouseDown(e, this.validTiles)}
           id="boardCanvas"
