ac4216b7f76ad7d3823a10a35f6ebf7fcfdd1865
Micaela O'Herron <micaela.oherron@gmail.com>
2018-11-25 18:30:05 -0500

    added emojis

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,57 +1,98 @@
-import React from 'React';
-import {connect} from 'react-redux';
-import {postMessage} from '../store';
+import React from "React";
+import { connect } from "react-redux";
+import { postMessage } from "../store";
 import socket from "../socket";
+import EmojiPicker from "emoji-picker-react";
+import Emoji from "emoji-js";
+const emoji = new Emoji.EmojiConvertor();
 
 class Chat extends React.Component {
-    constructor() {
-        super();
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            newMessage: ''
-        }
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        //  this.props.submitMessage(this.state.newMessage);
-         socket.emit('newMessage', this.props.roomId, this.props.player, this.state.newMessage);
-         this.setState({newMessage: ''});
-    }
-    handleChange(e) {
-       this.setState({newMessage: e.target.value})
-    }
-    render () {
-        const getTime = (date) => {
-            return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`
-         }
-         console.log("date", )
-        return (
-            <div className="chat">
-              <h3>Group Chat:</h3>
-              <div>
-                  {this.props.messages.map(message => {
-                     return <p> {getTime(new Date(Date.now()))} {message[0].name}: {message[1]}</p>
-                  })}
-              </div>
-              <form onSubmit={this.handleSubmit}>
-                <input onChange={this.handleChange} placeholder="Type message here..." value={this.state.newMessage}/>
-                <button type="submit"> Send </button>
-              </form>
-            </div>  
-        )
-    }
- }
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.toggleEmoji = this.toggleEmoji.bind(this);
+    this.state = {
+      newMessage: "",
+      showEmojis: false
+    };
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    //  this.props.submitMessage(this.state.newMessage);
+    socket.emit(
+      "newMessage",
+      this.props.roomId,
+      this.props.player,
+      this.state.newMessage
+    );
+    this.setState({ newMessage: "" });
+  }
+  handleChange(e) {
+    this.setState({ newMessage: e.target.value });
+  }
 
- const mapStateToProps = (state) => ({
-    messages: state.messages, 
-    roomId: state.roomId,
-    player: state.player
- })
+  handleEmojiClick = (code, emojiPicked) => {
+    let emojiPic = emoji.replace_colons(`:${emojiPicked.name}:`);
 
- const mapDispatchToProps = (dispatch) => ({
-    submitMessage: (message) => dispatch(postMessage(message))
- })
+    this.setState({
+      newMessage: this.state.newMessage + emojiPic
+    });
+  };
 
+  toggleEmoji() {
+    const currentEmojiState = this.state.showEmojis;
+    this.setState({
+      showEmojis: !currentEmojiState
+    });
+  }
 
- export default connect(mapStateToProps, mapDispatchToProps)(Chat);
+  render() {
+    const getTime = date => {
+      return `${date.getHours()}:${("0" + date.getMinutes()).slice(-2)}`;
+    };
+
+    return (
+      <div className="chat">
+        <h3>Group Chat: </h3>
+        <div>
+          {this.props.messages.map(message => {
+            return (
+              <p>
+                {getTime(new Date(Date.now()))} {message[0].name}: {message[1]}
+              </p>
+            );
+          })}
+        </div>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            placeholder="Type message here..."
+            value={this.state.newMessage}
+          />
+          <span onClick={this.toggleEmoji}>🙂 </span>
+          <button type="submit"> Send </button>
+        </form>
+        {this.state.showEmojis ? (
+          <EmojiPicker onEmojiClick={this.handleEmojiClick} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  messages: state.messages,
+  roomId: state.roomId,
+  player: state.player
+});
+
+const mapDispatchToProps = dispatch => ({
+  submitMessage: message => dispatch(postMessage(message))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Chat);
