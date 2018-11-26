b9e7ac0fa6d27bca9ae0888812842009608a4ef2
Micaela O'Herron <micaela.oherron@gmail.com>
2018-11-26 17:12:02 -0500

    initial commit

--- client/components/gameOver.js
+++ client/components/gameOver.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 
-const gameOver = props => {
-  return <h1> Game Over </h1>;
-};
+class gameOver extends Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        return <h1> Game Over </h1>;
+      </div>
+    );
+  }
+}
 
 export default gameOver;
+
+// const mapStateToProps = state => {
+//   return {
+//       joinRoomErr: state.messages.joinRoomErr,
+//       player: state.game.player
+//   }
+// }
+
+// export default connect(mapStateToProps)(JoinRoom)
