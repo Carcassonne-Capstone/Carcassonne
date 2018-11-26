f232e398e1caa767c2fafdade10edb12257fa9e3
Alizah Lalani <alizah.lalani@gmail.com>
2018-11-26 17:10:34 -0500

    added 3d model

--- client/components/renderFuncs/createTile.js
+++ client/components/renderFuncs/createTile.js
@@ -1,6 +1,21 @@
 import * as THREE from "three";
 import {validMeepleRegion} from './checkValidMeeple'
 import {createEmptyMeeple} from './createMeeple'
+// import {MTLLoader, OBJLoader} from 'three-obj-mtl-loader'
+import GLTFLoader from 'three-gltf-loader';
+
+
+
+// mtlLoader.load('/models/tree/materials.mtl', (materials) => {
+//   materials.preload()
+//   objLoader.setMaterials(materials)
+//   objLoader.load('/models/tree/tree.obj', (object) => {
+//     object.position.x=0;
+//     object.position.y=0;
+    
+//     return object
+//   })
+// })
 
 export const createCube = (tileNode, x, y, addEmptyMeeples) => {
   const tile = tileNode.tile
@@ -45,6 +60,42 @@ export const createCube = (tileNode, x, y, addEmptyMeeples) => {
       }
     });
   }
+
+  let mtlLoader = new THREE.MTLLoader();
+
+  mtlLoader.load('/models/tree/materials.mtl', (materials) => {
+    console.log('MATERIALS', materials)
+    materials[0].preload()
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials[0])
+    objLoader.load('/models/tree/tree.obj', (object) => {
+      object.position.x=0;
+      object.position.y=0;
+      object.position.z=.2;
+      object.scale.x=.1;
+      object.scale.y=.1;
+      object.scale.z=.1;
+      object.rotation.x = Math.PI / 2;
+      console.log('MATERIALS2', materials)
+      console.log('objects', object)
+      cube.add(object)
+    })
+  })
+
+  // const loader = new GLTFLoader();
+
+  // loader.load('/models/treeGltf/model.gltf', (object) => {
+  //       object.position.x=0;
+  //       object.position.y=0;
+  //       object.position.z=.2;
+  //       object.scale.x=.1;
+  //       object.scale.y=.1;
+  //       object.scale.z=.1;
+  //       object.rotation.x = Math.PI / 2;
+        
+  //       console.log('object', object)
+  //       cube.add(object)
+  //   })
 
   return cube;
 };
